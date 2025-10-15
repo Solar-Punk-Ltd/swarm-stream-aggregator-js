6612936ed472530a806c5f1a3e66deb00a75645f
Levente Kiss <levente.kiss@solarpunk.buzz>
2025-10-15 14:29:01 +1300

    fix: numShardsInCluster is required for autosharding

--- src/waku/Waku.ts
+++ src/waku/Waku.ts
@@ -6,6 +6,7 @@ import { Logger } from '../libs/logger.js';
 import { getOptionalEnvVariable } from '../utils/common.js';
 
 const WAKU_CLUSTER_ID = 0;
+const WAKU_NUM_SHARDS_IN_CLUSTER = 8;
 const WAKU_STATIC_PEER = getOptionalEnvVariable('WAKU_STATIC_PEER');
 
 export class Waku {
@@ -53,6 +54,7 @@ export class Waku {
   private async createWakuLightNode(): Promise<LightNode> {
     const networkConfig = {
       clusterId: WAKU_CLUSTER_ID,
+      numShardsInCluster: WAKU_NUM_SHARDS_IN_CLUSTER,
     };
 
     const node = await createLightNode({
@@ -145,6 +147,7 @@ export class Waku {
   public createWakuEncoder(topicName: string): Encoder {
     const networkConfig = {
       clusterId: WAKU_CLUSTER_ID,
+      numShardsInCluster: WAKU_NUM_SHARDS_IN_CLUSTER,
     };
 
     const contentTopic = `/solarpunk-msrs/1/${topicName}/proto`;
@@ -238,7 +241,7 @@ export class Waku {
         // Network configuration
         networkConfig: {
           clusterId: WAKU_CLUSTER_ID,
-          numShardsInCluster: 8,
+          numShardsInCluster: WAKU_NUM_SHARDS_IN_CLUSTER,
         },
 
         // Node configuration
